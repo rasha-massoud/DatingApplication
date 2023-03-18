5b2f30a1f01330d803936f00514edd3be5cf6f3a
RashaM <rashamassoud1998@gmail.com>
2023-03-18 21:46:55 +0200

    Added the profiles' table to the database

---
+++ BE/database/migrations/2023_03_18_194222_create_profiles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("user_id");
+            $table->text("optional_profile1");
+            $table->text("optional_profile2");
+            $table->text("optional_profile3");
+            $table->timestamps();
+
+            $table->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
