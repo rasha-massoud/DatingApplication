6f82258f2546eb0199032a33125d1a27941c9834
RashaM <rashamassoud1998@gmail.com>
2023-03-19 18:00:22 +0200

    Created the link between the login and registration pages

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -9,10 +9,8 @@ use App\Http\Controllers\AuthController;
 Route::group(["prefix" => "v0.0.0"], function(){
     Route::group(["prefix" => "user"], function(){
         Route::post('/register',[AuthController:: class, "register"]);
-
-
-        // Route::post('/registration',[UserController:: class, "registration"]);
-        // Route::post('/',[UserController:: class, "login"]);
-
+        Route::post('/',[AuthController:: class, "login"]);
+        Route::post('/logout',[AuthController:: class, "logout"]);
+        Route::post('/refresh',[AuthController:: class, "refresh"]);
     });
 });
