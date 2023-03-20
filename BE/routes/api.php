e9effe9cf542fe98b087482a3828dd1e54b9afd7
RashaM <rashamassoud1998@gmail.com>
2023-03-20 19:59:26 +0200

    Added the link to the search controller from the api.php

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SearchController;
 
 Route::group(["prefix" => "v0.0.0"], function(){
     Route::post('/register',[AuthController:: class, "register"]);
