c6336d41e0b635e9e2c415f94469a6535050bbe7
RashaM <rashamassoud1998@gmail.com>
2023-03-21 23:58:30 +0200

    Modified the Favorite and Block lists to return the names as well

--- BE/app/Http/Controllers/ListController.php
+++ BE/app/Http/Controllers/ListController.php
@@ -12,15 +12,21 @@ class ListController extends Controller
 {
     function blocklist(Request $request){
         $blocks = Block::where('user_id', $request->user_id)->get();
+        $blockedUserIds = $blocks->pluck('blocked_user_id');
+        $blockedUsers = User::whereIn('id', $blockedUserIds)->get();        
         return response()->json([
-            "blocks" => $blocks
+            "blocks" => $blocks,
+            "blockedUsers" => $blockedUsers
         ]);
     }
 
     function favoriteList(Request $request){
         $favorites = Favorite::where('user_id', $request->user_id)->get();
+        $favoriteUserIds = $favorites->pluck('favorite_user_id');
+        $favoriteUsers = User::whereIn('id', $favoriteUserIds)->get(); 
         return response()->json([
-            "favorites" => $favorites
+            "favorites" => $favorites,
+            "favoriteUsers" => $favoriteUsers
         ]);
     }
 }
