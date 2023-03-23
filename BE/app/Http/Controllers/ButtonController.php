d19f22637f26abf7601c08cbd41dac8ac38f7c00
RashaM <rashamassoud1998@gmail.com>
2023-03-23 13:48:35 +0200

    Added the notification update to the favorite and block actions

--- BE/app/Http/Controllers/ButtonController.php
+++ BE/app/Http/Controllers/ButtonController.php
@@ -26,19 +26,28 @@ class ButtonController extends Controller
 
         if($condition){
             $response['status'] = "fails";
+            $response['action'] = "no action";
         }
         else{
             $block= new Block;
 
             $block->user_id = $request->user_id;
             $block->blocked_user_id = $request->blocked_user_id;
-    
             $block->save();
+
+            $notification = new Notification;
+            $notification->user_id = $request->user_id;
+            $notification->on_user_id = $request->blocked_user_id;
+            $notification->action = "block";
+            $notification->save();
+
             $response['status'] = "success";
+            $response['action'] = "block";
         }
 
         return response()->json([
-            'status' => $response['status']
+            'status' => $response['status'],
+            'action' => $response['action'],
         ]);   
     }
 
@@ -53,6 +62,7 @@ class ButtonController extends Controller
 
         if($condition){
             $response['status'] = "fails";
+            $response['action'] = "no action";
         }
         else{
             $favorite= new Favorite;
@@ -61,12 +71,19 @@ class ButtonController extends Controller
             $favorite->favorite_user_id = $request->favorite_user_id;   
             $favorite->save();
 
+            $notification = new Notification;
+            $notification->user_id = $request->user_id;
+            $notification->on_user_id = $request->favorite_user_id;
+            $notification->action = "favorite";
+            $notification->save();
 
             $response['status'] = "success";
+            $response['action'] = "favorite";
         }
         
         return response()->json([
-            'status' => $response['status']
+            'status' => $response['status'],
+            'action' => $response['action'],
         ]);   
     }
 
